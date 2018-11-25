3c0af29d8ea4be382b99d73b6dbdb8acee64a7a5
danibots <danielle.pepin@gmail.com>
2018-11-25 17:39:15 +0000

    add array for numbers and create while loop to create views in Numbers

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +29,30 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Add array for numbers
+        ArrayList<String> words = new ArrayList<String>();
+
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+        //Add text views for each item in ArrayList words
+        int index = 0;
+
+        while (index < words.size()){
+
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+
+            index++;
+
+
+        }
     }
 }
